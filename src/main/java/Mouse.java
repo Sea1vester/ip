e164e475a9340115176dd699120fe1ad4ebccd33
Sylvester <sylvester0928@gmail.com>
2026-08-18 12:44:29 +0800

    Change Duke to Mouse

---
+++ src/main/java/Mouse.java
@@ -0,0 +1,13 @@
+/**
+ * Entry point for the Mouse chatbot.
+ */
+public class Mouse {
+    public static void main(String[] args) {
+        String banner = " __  __                      \n"
+                + "|  \\/  | ___  _   _ ___  ___ \n"
+                + "| |\\/| |/ _ \\| | | / __|/ _ \\\n"
+                + "| |  | | (_) | |_| \\__ \\  __/\n"
+                + "|_|  |_|\\___/ \\__,_|___/\\___|\n";
+        System.out.println(banner);
+    }
+}
