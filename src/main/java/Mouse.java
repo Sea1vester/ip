3af8e3239761f3a6c70340654618440d7e8285f9
Sylvester <sylvester0928@gmail.com>
2026-08-18 12:55:13 +0800

    Create Greeting

--- src/main/java/Mouse.java
+++ src/main/java/Mouse.java
@@ -3,11 +3,24 @@
  */
 public class Mouse {
     public static void main(String[] args) {
+        printGreeting();
+    }
+
+    public static void printGreeting() {
         String banner = " __  __                      \n"
                 + "|  \\/  | ___  _   _ ___  ___ \n"
                 + "| |\\/| |/ _ \\| | | / __|/ _ \\\n"
                 + "| |  | | (_) | |_| \\__ \\  __/\n"
                 + "|_|  |_|\\___/ \\__,_|___/\\___|\n";
-        System.out.println(banner);
+        String greeting = "____________________________________________________________\n"
+        + banner + "\n"
+        + "Hello! I'm Mouse.\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________\n";
+        System.out.println(greeting);
     }
 }
+
+
